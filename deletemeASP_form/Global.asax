1dabbd6d098e3ff8e59b4df6223c7a4fc68ec85f
Phil Anderson <philanderson888@hotmail.com>
2020-01-13 16:39:15 +0000

    tidying up labs

--- deletemeASP_form/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="deletemeASP_form.Global" Language="C#" %>
--- lab_hw_106_interview_prep/Program.cs
+++ lab_hw_106_interview_prep/Program.cs
@@ -14,35 +14,5 @@ namespace lab_hw_106_interview_prep
         }
     }
 
-    public class ASCII
-    {
-        // Goal : Return ASCII number of character sent in
-        // Input a string eg "hello" and an index eg 3
-        // Return ASCII number of letter 'l'
-        public int ASCII_Return_Index_Of_String(string input, int index)
-        {
-            // a string is already a char array 
-            char[] charArray = input.ToCharArray();
-            var selectedChar = charArray[index];
-            var ASCIIcode = (int)selectedChar;
-            return ASCIIcode;
-        }
-    }
-
-    public class ThrowException
-    {
-        public int Throw_Exception_Test(int x, int y)
-        {
-            int output = 0;
-            checked
-            {
-                output = x * y;
-              
-            }
-            return output;
-        }
-    }
-
-
 
 }
