c5f95b7abd66aa54d49ff3e0538a8a62022b2042
Phil Anderson <philanderson888@hotmail.com>
2020-01-20 16:58:35 +0000

    Updating 2019-04-labs

--- lab_42_testme/TestMe.cs
+++ lab_42_testme/TestMe.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,14 +6,5 @@ using System.Threading.Tasks;
 
 namespace lab_42_testme
 {
-    public class TestMe
-    {
-        public double BIDMAS01(int x, int y, int z)
-        {
-            // goal : x + y/z observing BIDMAS rules
-            double Y = y;
-            double Z = z;
-            return x + (Y/Z);   // -1 means 'fail'
-        }
-    }
+
 }
